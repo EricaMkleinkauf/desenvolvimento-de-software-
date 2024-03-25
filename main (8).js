27731872c5ac64468720b6f24e21f1cba806f921
EricaMkleinkauf <162368774+EricaMkleinkauf@users.noreply.github.com>
2024-03-25 17:05:05 -0300

    Add files via upload

---
+++ main (8).js
@@ -0,0 +1,51 @@
+var a = 15;
+var b = 5;
+
+if ((a > 10 && a < 20) || b ==5)
+print ("true");
+else 
+ print("false");
+ 
+ 
+ var a = 15;
+ var b = 5;
+ 
+ if ((a > 10) || b ==5)
+ print ("true");
+ else 
+ print("false")
+ 
+ 
+ var a = 15;
+ var b = 5;
+ var c = 10;
+ 
+ if ((a > 10 && a < 20) || b ==5 || c==10)
+ print ("true")
+ else
+ print ("false")
+ 
+ 
+ 
+ var a = 15;
+ var b = 5;
+ var c = 10;
+ 
+ if(((a > 10) || b ==5) && c!=10)
+ print ("true")
+else
+print ("false")
+
+
+ 
+ var a = 15;
+ var b = 5;
+ var c = 10;
+ var d = 10;
+ if((a > 10 || b == 5) && c!=10 && d!=20)
+ print ("true");
+ else
+ print ("false")
+
+ 
+ 
